b81a49fadc7b92de2ef54f26015b2ea5376c087c
Stelios Baglaridis <stelios@baglaridis.com>
2017-10-26 21:20:52 +0000

    Server is working! + added mime.js for mime-types

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const http = require("http");
+const fs = require('fs');
+const path = require('path');
+
 const server = new http.Server();
+
+const mime = require('./server/mime');
+
+const rootDir = './client';
+const indexFile = '/index.html';
 
 server.listen(process.env.PORT, process.env.IP, () => {
 
@@ -7,9 +15,41 @@ server.listen(process.env.PORT, process.env.IP, () => {
 });
 
 server.on('request', (request, response) => {
-    
+
     console.log('========== new request to', request.url);
-    
-    response.end('Server is up & running!');
-    
+
+    let url = (request.url === '/') ? indexFile : request.url;
+        url = rootDir + url;
+
+    new Promise((resolve, reject) => {
+
+        fs.stat(url, (error, stat) => {
+
+            if (error !== null) {
+
+                return reject(url);
+            }
+
+            stat.isFile() ? resolve(url) : reject(url);
+        });
+    })
+        .then(() => ({ path: url, code: 200 }))
+        .catch(() => ({ path: `${rootDir}/404.html`, code: 404 }))
+        .then(info => {
+
+            const ext = path.extname(info.path);
+
+            // FS READ THE FILE
+            fs.readFile(info.path, (error, data) => {
+
+                if (error) {
+                    response.writeHead(404, { 'Content-Type': mime['.html'] });
+                    response.end(`fs.readFile error: ${info.path}`);
+                    return false;
+                }
+
+                response.writeHead(info.code, { 'Content-Type': mime[ext] });
+                response.end(data);
+            });
+        });
 })
---
+++ server/mime.js
@@ -0,0 +1,5 @@
+module.exports = {
+    '.html': 'text/html; charset=urf-8',
+    '.css': 'text/css; charset=urf-8',
+    '.js': 'application/javascript; charset=urf-8'
+};
